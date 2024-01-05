ad89855104d2bca507094050d3fd98540ca712ea
hefengbao <754582231@qq.com>
2024-01-05 11:47:08 +0800

    数据库版本升级到 6

--- app/src/main/java/com/hefengbao/jingmo/data/database/AppDatabase.kt
+++ app/src/main/java/com/hefengbao/jingmo/data/database/AppDatabase.kt
@@ -39,12 +39,13 @@ import com.hefengbao.jingmo.data.database.util.DetailInfoListConverter
         TongueTwisterEntity::class,
         WriterEntity::class,
     ],
-    version = 5,
+    version = 6,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
         AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4),
         AutoMigration(from = 4, to = 5),
+        AutoMigration(from = 5, to = 6),
     ],
     exportSchema = true
 )
