b01bc80ae50e09c9edaa62fdb30d9195e657fa27
hefengbao <754582231@qq.com>
2024-01-13 15:17:30 +0800

    暂时没用

---
+++ app/src/main/java/com/hefengbao/jingmo/data/enums/writing/CommentType.kt
@@ -0,0 +1,9 @@
+package com.hefengbao.jingmo.data.enums.writing
+
+enum class CommentType(val type: String){
+    Text("Text"),
+    CharDictInJson("CharDictInJson"),
+    WordDictInJson("WordDictInJson"),
+    AllusionKey("AllusionKey"),
+    Image("Image"),
+}
