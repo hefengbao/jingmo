565426089ff5e782651b835bd5ad82cf209b7337
hefengbao <754582231@qq.com>
2024-01-15 14:12:30 +0800

    重新定义pref名称

--- app/src/main/java/com/hefengbao/jingmo/common/Constant.kt
+++ app/src/main/java/com/hefengbao/jingmo/common/Constant.kt
@@ -2,7 +2,7 @@ package com.hefengbao.jingmo.common
 
 object Constant {
     val DB_NAME = "jingmo.db"
-    val DATASTORE_NAME = "jingmo.ds"
-    val DATASTORE_DATASET_NAME = "jingmo_dataset.ds"
-    val DATASTORE_READ_STATUS_NAME = "jingmo_read_status.ds"
+    val DATASTORE_NAME = "jingmo"
+    val DATASTORE_DATASET_NAME = "jingmo_dataset"
+    val DATASTORE_READ_STATUS_NAME = "jingmo_read_status"
 }
