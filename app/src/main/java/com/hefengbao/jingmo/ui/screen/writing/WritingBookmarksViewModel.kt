6ad684e89d3526cf3b506a72fb399bc2d5451b09
hefengbao <754582231@qq.com>
2024-07-03 09:27:24 +0800

    #62 fix:Attempt to collect twice from pageEventFlow

--- app/src/main/java/com/hefengbao/jingmo/ui/screen/writing/WritingBookmarksViewModel.kt
+++ app/src/main/java/com/hefengbao/jingmo/ui/screen/writing/WritingBookmarksViewModel.kt
@@ -1,6 +1,8 @@
 package com.hefengbao.jingmo.ui.screen.writing
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.hefengbao.jingmo.data.repository.WritingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,5 +11,5 @@ import javax.inject.Inject
 class WritingBookmarksViewModel @Inject constructor(
     writingRepository: WritingRepository
 ) : ViewModel() {
-    val writings = writingRepository.collections()
+    val writings = writingRepository.collections().cachedIn(viewModelScope)
 }
