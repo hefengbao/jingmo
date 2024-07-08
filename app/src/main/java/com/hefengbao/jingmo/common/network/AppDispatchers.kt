a3190a60134fd6eda1762672e5dfc0e4dbf8c46e
hefengbao <754582231@qq.com>
2024-07-08 18:26:52 +0800

    add Copyright

--- app/src/main/java/com/hefengbao/jingmo/common/network/AppDispatchers.kt
+++ app/src/main/java/com/hefengbao/jingmo/common/network/AppDispatchers.kt
@@ -1,17 +1,10 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * This file is part of the 京墨（jingmo）APP.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * (c) 贺丰宝（hefengbao） <hefengbao@foxmail.com>
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 package com.hefengbao.jingmo.common.network
--- app/src/main/java/com/hefengbao/jingmo/data/model/Riddle.kt
+++ app/src/main/java/com/hefengbao/jingmo/data/model/Riddle.kt
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the 京墨（jingmo）APP.
+ *
+ * (c) 贺丰宝（hefengbao） <hefengbao@foxmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 package com.hefengbao.jingmo.data.model
 
 import com.hefengbao.jingmo.data.database.entity.RiddleEntity
