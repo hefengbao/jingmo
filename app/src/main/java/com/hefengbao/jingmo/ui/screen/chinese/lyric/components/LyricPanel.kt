07ee306496e75b0245c76cc2826516cc16f0e2f5
hefengbao <754582231@qq.com>
2024-07-17 11:15:47 +0800

    update: 优化显示

--- app/src/main/java/com/hefengbao/jingmo/ui/screen/chinese/lyric/components/LyricPanel.kt
+++ app/src/main/java/com/hefengbao/jingmo/ui/screen/chinese/lyric/components/LyricPanel.kt
@@ -10,6 +10,7 @@
 package com.hefengbao.jingmo.ui.screen.chinese.lyric.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,8 +18,8 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.hefengbao.jingmo.data.database.entity.chinese.LyricEntity
 
@@ -35,36 +36,38 @@ fun LyricPanel(
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             item {
-                Text(
-                    modifier = modifier.fillMaxWidth(),
-                    text = entity.title,
-                    style = MaterialTheme.typography.titleMedium,
-                    textAlign = TextAlign.Center
-                )
-            }
-            item {
-                var text = ""
-                if (entity.writer != null) {
-                    text += "填词：${entity.writer}"
-                }
-                if (entity.singer != null) {
-                    if (text.isNotEmpty()) {
-                        text += " / "
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = entity.title,
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+
+                    var text = ""
+                    if (entity.writer != null) {
+                        text += "填词：${entity.writer}"
                     }
-                    text += "演唱：${entity.singer}"
+                    if (entity.singer != null) {
+                        if (text.isNotEmpty()) {
+                            text += " / "
+                        }
+                        text += "演唱：${entity.singer}"
+                    }
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.labelMedium.copy(
+                            brush = null,
+                            alpha = .5f
+                        ),
+                    )
+                    Text(
+                        text = entity.content,
+                    )
                 }
-                Text(
-                    modifier = modifier.fillMaxWidth(),
-                    text = text,
-                    style = MaterialTheme.typography.labelMedium.copy(brush = null, alpha = .5f),
-                    textAlign = TextAlign.Center
-                )
-            }
-            item {
-                Text(
-                    modifier = modifier.fillMaxWidth(),
-                    text = entity.content,
-                )
+
             }
         }
     }
