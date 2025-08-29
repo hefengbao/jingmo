42d7254baff340be9b4f93688d25e60f46f72512
hefengbao <754582231@qq.com>
2025-08-29 14:48:26 +0800

    v1.15.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.hefengbao.jingmo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1_015_001
-        versionName = "1.15.1"
+        versionCode = 1_015_002
+        versionName = "1.15.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
